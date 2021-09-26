a0a0b19c9621bfd0c4bcf7c46f3435ab7dcbb17f
Kyle LeBlanc <53625838+tiberiusnero560@users.noreply.github.com>
2021-09-26 16:39:07 -0400

    Complete
    Basic functions complete with API calls and ability to type and have that user displayed using the github users api
    
    Experimenting
    
    playing with different things and the responses that I get to begin to get a deeper understanding of HTTP requests and how to interface with the APIs more comfortably. I want to be able to effortlessly interface with these APIs at a moments notice.

---
+++ src/API/GetData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GetData() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default GetData
--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import { axios } from 'axios'
+
 
 function App() {
+
+  // const [ response, setResponse ] = useState('');
+  const [ data, setData ] = useState('');
+  const [name, setName] = useState('');
+  const [userName, setUsername] = useState('');
+  const [followers, setFollowers] = useState('');
+  const [following, setFollowing] = useState('');
+  const [repos, setRepos] = useState('');
+  // const [avatar, setAvatar] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [error, setError] = useState(null);
+  const [allUsers, setAllUsers] = useState('');
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    const response = await fetch(`http://api.github.com/users`);
+    const API__data = await response.json();
+    API__data.map((data, index, API__data) => { 
+      
+      console.log(setAllUsers(data.login))
+    })
+  }
+
+  
+
+  const set__Data = ({
+    name,
+    login,
+    followers,
+    following,
+    public_repos,
+    avatar_url
+  }) => {
+    setName(name);
+    setUsername(login);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepos(public_repos);
+    // setAvatar(avatar_url)
+  }  
+
+
+  const handleSearch = (e) => {
+    setUserInput(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    fetch(`https://api.github.com/users/${userInput}`)    // This is the event handler that allows me to type and return the user from the Github APi. but make sure to add the onclick in the return
+      .then(res => res.json())
+      .then(data => {
+        if (data.message) {
+          setError(data.message)
+        } else {
+          setData(data)
+          setError(null)
+        }
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='navbar'>Job Interview React Challenge </div>
+
+      <div className='All__Users'>
+      </div>
+
+
+      {/* Search Container  */}
+      <div className='search'>
+        <form onChange={handleSearch}>
+          
+            <form placeholder='Github User' name='github user'  />
+            <form content='Submit' onSubmit={handleSubmit}/>
+
+         
+        </form>
+      </div> {/* Search Container End Div  */}
+
+      { error ? (<h1>{error}</h1>) : 
+      (<div className='card'>  
+        <input  />
+        <input />
+
+        <div className='card__item'>
+          <a>
+            {followers} Followers
+          </a>
+        </div>
+
+        <div className='card__item'>
+          <a>
+            {repos} Repos
+          </a>
+        </div>
+
+        <div className='card__item'>
+          <a>
+            {following} Following
+          </a>
+        </div>
+        
+  
+      
+        
+      </div>
+      )}
+      {/* Card Container  */}
+      
+
     </div>
   );
 }
